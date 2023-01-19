9e55041bcc8f8c1d4484b9299d501a3e0607b086
Mohsin Asghar <mohsinasghar0876@gmail.com>
2023-01-19 15:13:55 +0500

    change URL

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,8 +4,9 @@ import GithubProvider from 'next-auth/providers/github';
 export const authOptions = {
     providers: [
         GithubProvider({
-            clientId : "9d058508b9c53ec69976",
-            clientSecret: "6ed87b4c2fd4e054ac52f60ca09210694013e783"
+            clientId : process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+            secret: process.env.SECRET
         }),
     ],
 };
