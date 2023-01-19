48358c5feb646bd7ce514f5b8188de6134dd71df
Mohsin Asghar <mohsinasghar0876@gmail.com>
2023-01-19 16:06:08 +0500

    modified env file

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,12 @@
 import NextAuth from "next-auth";
 import GithubProvider from 'next-auth/providers/github';
 
-export const authOptions = {
+export default NextAuth({
     providers: [
         GithubProvider({
             clientId : process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
-            secret: process.env.SECRET
+            clientSecret: process.env.GITHUB_SECRET
         }),
     ],
-};
- export default NextAuth(authOptions);
+    secret: process.env.JWT_SECRET
+});
