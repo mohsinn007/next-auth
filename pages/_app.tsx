5da6e1c7e4d8d24324c12c63b1ecda1103461718
Mohsin Asghar <mohsinasghar0876@gmail.com>
2023-01-19 16:26:04 +0500

    add custom route in URL

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import { SessionProvider } from 'next-auth/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-      <SessionProvider session={pageProps.session}>
+      <SessionProvider session={pageProps.session} basePath='/custom-route/api/auth'>
         <Navbar />
         <Component {...pageProps} />
       </SessionProvider>
